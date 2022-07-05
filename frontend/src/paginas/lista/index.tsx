f4eb779e1f1fecc153e06e36b9ca4fe5f3bd9e0b
felipedba <felipedba2020@gmail.com>
2022-07-05 18:49:43 -0300

    Atualização para nuvem (Netlify)

--- frontend/src/paginas/lista/index.tsx
+++ frontend/src/paginas/lista/index.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import { Modal } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { SelectItens } from "utils/selectItens";
 import MinhasListas from "./minhasListas";
 import './Style.css';
 
 export default function Lista() {
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+
     const historys = useNavigate();
     const cpfLogado = localStorage.getItem('CafeManha')
 
@@ -23,38 +18,20 @@ export default function Lista() {
                 <h2 className="centralizar_h2">
                     Meus Itens <br />
                 </h2>
-                <div>
-                    <SelectItens />
 
-                    <button type="button" className="btnPersonalLista"> Inserir</button>
-                    <button type="button" className="btnPersonalLista" onClick={handleShow}> Atualizar</button>
-                    <Modal
-                        show={show}
-                        onHide={handleClose}
-                        backdrop="static"
-                        keyboard={false}>
-                        <Modal.Header>
-                            <Modal.Title></Modal.Title>
-                            <button type="button"
-                                className="close btnPersonal"
-                                onClick={handleClose}
-                                aria-label="Close">
-                                Cancelar
-                            </button>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <SelectItens />
-                            <button type="button" className="btnPersonalLista"> Atualizar</button>
-                        </Modal.Body>
-                    </Modal>
+                <select>
+                    <option value="queijo"> Queijo prato</option>
+                    <option value="queijo"> Queijo Coalho</option>
+                </select>
 
+                <button type="button" className="btnPersonalLista"> Inserir</button>
+                <button type="button" className="btnPersonalLista"> Atualizar</button>
+                <button type="button"
+                    className="btnPersonalLista"
+                    onClick={sair}>
+                    Sair
+                </button>
 
-                    <button type="button"
-                        className="btnPersonalLista"
-                        onClick={sair}>
-                        Sair
-                    </button>
-                </div>
                 <MinhasListas estadoBotao={false}
                     urlDoBanco={`/pageCpf?cpf=${cpfLogado}`} />
 
